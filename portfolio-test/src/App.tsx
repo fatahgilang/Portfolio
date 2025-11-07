c284834a9eb9cd61edc32edd1ce7dcbd68bc168d
gilang <fatahgilang2@gmail.com>
2025-11-07 16:00:16 +0700

    6

---
+++ portfolio-test/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Portfolio Test</h1>
+        <p>This is a simple test to verify GitHub Pages deployment.</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
